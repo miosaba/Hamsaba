d44b1b49456d8b2a5f9d541024fe28328397f880
miosaba <68880506+miosaba@users.noreply.github.com>
2020-07-28 20:17:07 +0900

    Add files via upload

---
+++ hambot/discordbot.py
@@ -0,0 +1,37 @@
+#インストールした discord.py を読み込む
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NzM3Mjk5NTQzMDc0Mjc1MzM4.Xx7VtQ.7mpbHMMYlVA2lhNMYlXHx8qhrh0'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    elif message.content.startswith('@HamterRAW'):#文字列の開始が一致
+        await message.channel.send('おい呼ばれてるぞHamterRAW')
+    elif message.content =='ハム':
+        await message.channel.send('ローストハム')
+    elif '太郎' in message.content:
+        await message.channel.send('ヘケッ')
+    elif message.content =='Ham':
+        await message.channel.send('大好きなのは血だまりと金')
+    elif message.content =='HamterRAW':
+        await message.channel.send('ヘッドフォンぶん投げHamterRAW')
+    elif message.content =='嫌い':
+        await message.channel.send('Ham好きkillyou')
+    elif message.content =='ham':
+        await message.channel.send('あほのham')
+    elif 'はむカービィ'in message.content:
+        await message.channel.send('俺はむらびと使い')
+#Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
